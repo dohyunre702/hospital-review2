24038501e5f3963b41ec33869483d316a0e62eca
dohyu <dohyunre702@gmail.com>
2022-11-28 15:19:38 +0900

    [update] UserService - orElseThrow 참고용

--- src/main/java/com/example/hospital/review/domain/dto/UserService.java
+++ src/main/java/com/example/hospital/review/domain/dto/UserService.java
@@ -12,13 +12,18 @@ public class UserService {
     public UserDto join(UserJoinRequest request) {
         // 비즈니스 로직 - 회원 가입
         // 회원 userName(id) 중복 check
-        // 중복이면 회원가입 x -> Exception(예외) 발생해 처리 필요
+        // 중복이면 회원가입 x -> Exception(예외) 발생
+        /*
+        userRepository.findByUserName(request.getUserName())
+                .orElseThrow(() -> new RuntimeException("해당 UserName이 중복됩니다."));
+         */
 
+        // 있으면 에러처리
         userRepository.findByUserName(request.getUserName())
                 .ifPresent(user -> {
                     throw new RuntimeException("해당 UserName이 중복됩니다.");
                 });
-        
+
         //회원가입 .save()
         User savedUser = userRepository.save(request.toEntity());
         return UserDto.builder()
