dfd1b40255d74fbe2ef94df707fa4c80d061196d
dohyu <dohyunre702@gmail.com>
2022-11-28 15:10:27 +0900

    [update] UserDto - Builder, id 추가

--- src/main/java/com/example/hospital/review/domain/dto/UserDto.java
+++ src/main/java/com/example/hospital/review/domain/dto/UserDto.java
@@ -2,12 +2,13 @@ package com.example.hospital.review.domain.dto;
 
 import lombok.*;
 
+@Builder
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
 public class UserDto {
+    private Long id;
     private String userName;
     private String password;
     private String emailAddress;
-
 }
