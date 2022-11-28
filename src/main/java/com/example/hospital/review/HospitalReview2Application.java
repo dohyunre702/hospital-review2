8ab7f4a5c904b6fe77e0c127ff3d0ebbe8bda6e1
dohyu <dohyunre702@gmail.com>
2022-11-28 13:23:02 +0900

    first commit

---
+++ src/main/java/com/example/hospital/review/HospitalReview2Application.java
@@ -0,0 +1,13 @@
+package com.example.hospital.review;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HospitalReview2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HospitalReview2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/hospital/review/HospitalReview2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hospital.review;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospitalReview2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
