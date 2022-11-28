00804ca57e435c90b6fbdeea9e9f269a92ffe346
dohyu <dohyunre702@gmail.com>
2022-11-28 14:15:14 +0900

    [add] UserController

---
+++ src/main/java/com/example/hospital/review/controller/UserController.java
@@ -0,0 +1,19 @@
+package com.example.hospital.review.controller;
+
+import com.example.hospital.review.domain.Response;
+import lombok.*;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/users")
+@RequiredArgsConstructor
+public class UserController {
+    private final UserService userService;
+
+    @PostMapping
+    public Response<UserJoinResponse> join (@RequestBody UserJoinRequest userJoinRequest) {
+        UserDto userDto = userService.join(userJoinRequest);
+        return Response.success(new UserJoinResponse(userDto.getUserName(), userDto.getEmailAddress()));
+    }
+
+}
