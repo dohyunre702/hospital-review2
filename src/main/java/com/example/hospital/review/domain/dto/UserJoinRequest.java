cf2cd0c6a27528a47fd81afd2f65d22a5cd8c22f
dohyu <dohyunre702@gmail.com>
2022-11-28 15:09:11 +0900

    [update] UserJoinRequest - .toEntity() 추가

--- src/main/java/com/example/hospital/review/domain/dto/UserJoinRequest.java
+++ src/main/java/com/example/hospital/review/domain/dto/UserJoinRequest.java
@@ -1,5 +1,6 @@
 package com.example.hospital.review.domain.dto;
 
+import com.example.hospital.review.domain.User;
 import lombok.*;
 
 @Getter
@@ -9,4 +10,12 @@ public class UserJoinRequest {
     private String userName;
     private String password;
     private String emailAddress;
+
+    public User toEntity() {
+        return User.builder()
+                .userName(this.userName)
+                .password(this.password)
+                .emailAddress(this.emailAddress)
+                .build();
+    }
 }
