eac560b8979fcc6c8c893fe0b1ea4892963a0c72
dohyu <dohyunre702@gmail.com>
2022-11-29 13:23:28 +0900

    [update] UserService - 에러처리 수정 - .save() 수정: token으로 받기

--- src/main/java/com/example/hospital/review/service/UserService.java
+++ src/main/java/com/example/hospital/review/service/UserService.java
@@ -3,14 +3,18 @@ package com.example.hospital.review.service;
 import com.example.hospital.review.domain.User;
 import com.example.hospital.review.domain.dto.UserDto;
 import com.example.hospital.review.domain.dto.UserJoinRequest;
+import com.example.hospital.review.exception.ErrorCode;
+import com.example.hospital.review.exception.HospitalReviewAppException;
 import com.example.hospital.review.repository.UserRepository;
 import lombok.*;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class UserService {
     private final UserRepository userRepository;
+    private final BCryptPasswordEncoder encoder;
     public UserDto join(UserJoinRequest request) {
         // 비즈니스 로직 - 회원 가입
         // 회원 userName(id) 중복 check
@@ -23,11 +27,11 @@ public class UserService {
         // 있으면 에러처리
         userRepository.findByUserName(request.getUserName())
                 .ifPresent(user -> {
-                    throw new RuntimeException("해당 UserName이 중복됩니다.");
+                    throw new HospitalReviewAppException(ErrorCode.DUPLICATED_USER_NAME, String.format("UserName:%s", request.getUserName()));
                 });
 
         //회원가입 .save()
-        User savedUser = userRepository.save(request.toEntity());
+        User savedUser = userRepository.save(request.toEntity(encoder.encode(request.getPassword())));
         return UserDto.builder()
                 .id(savedUser.getId())
                 .userName(savedUser.getUserName())
