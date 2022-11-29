ea056db133dcaf480f1f4598ddc3b82d5fe7bf0a
dohyu <dohyunre702@gmail.com>
2022-11-29 15:35:33 +0900

    [update] ErrorCode 수정 - 로그인 실패 케이스 추가(conflict, not_found, bad_request)

--- src/main/java/com/example/hospital/review/exception/ErrorCode.java
+++ src/main/java/com/example/hospital/review/exception/ErrorCode.java
@@ -6,8 +6,9 @@ import org.springframework.http.HttpStatus;
 @AllArgsConstructor
 @Getter
 public enum ErrorCode {
-    DUPLICATED_USER_NAME(HttpStatus.CONFLICT, "User name is duplicated.");
-    //.mR
+    DUPLICATED_USER_NAME(HttpStatus.CONFLICT, "Username is duplicated."),
+    NOT_FOUND(HttpStatus.NOT_FOUND, "Username Not Found."),
+    INVALID_PASSWORD(HttpStatus.BAD_REQUEST, "");
     private HttpStatus status;
     private String message;
 }
