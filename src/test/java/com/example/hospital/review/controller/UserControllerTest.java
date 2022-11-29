b68ffef69b8bd7e44e02bb77c4d0a15f08ff056b
dohyu <dohyunre702@gmail.com>
2022-11-29 13:31:42 +0900

    [update] UserControllerTest 수정 - join_success Method에 @withMockUser 추가

--- src/test/java/com/example/hospital/review/controller/UserControllerTest.java
+++ src/test/java/com/example/hospital/review/controller/UserControllerTest.java
@@ -14,11 +14,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -41,23 +43,24 @@ public class UserControllerTest {
 
     @Test
     @DisplayName("회원가입 성공")
+    @WithMockUser
     void join_success() throws Exception {
         UserJoinRequest userJoinRequest = UserJoinRequest.builder()
                 .userName("dd")
                 .password("1q2w3e4r")
-                .email("ddhlee@gamil.com")
+                .emailAddress("ddhlee@gamil.com")
                 .build();
 
         when(userService.join(any())).thenReturn(mock(UserDto.class));
 
         mockMvc.perform(post("/api.v1/users/join")
+                        .with(csrf())
                 .contentType(MediaType.APPLICATION_JSON)
 
                 .content(objectMapper.writeValueAsBytes(userJoinRequest)))
                 .andDo(print())
                 .andExpect(status().isOk());
     }
-
 
     @Test
     @DisplayName("회원가입 실패")
@@ -66,7 +69,7 @@ public class UserControllerTest {
         UserJoinRequest userJoinRequest = UserJoinRequest.builder()
                 .userName("dd")
                 .password("1q2w3e4r")
-                .email("ddhlee@gamil.com")
+                .emailAddress("ddhlee@gamil.com")
                 .build();
 
         when(userService.join(any())).thenThrow(new HospitalReviewAppException(ErrorCode.DUPLICATED_USER_NAME, ""));
@@ -79,4 +82,4 @@ public class UserControllerTest {
                 .andExpect(status().isConflict());
         }
     }
-}
+
